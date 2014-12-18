701b7ea5175571ff93ccda8edcee2f3ab3da8ae6
303350114 <zhantao.feng@gmail.com>
2014-12-18 15:31:03 +1300

    Integrate Primefaces 5.1 with Hibernate 4.3.7 and Spring 4.1.3.

---
+++ ordersys/leyoho-ordersys-web/src/main/java/com/leyoho/ordersys/entity/Aa.java
@@ -0,0 +1,23 @@
+package com.leyoho.ordersys.entity;
+
+import org.apache.commons.collections.comparators.ComparableComparator;
+public class Aa {
+
+	public int id;
+	public String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
